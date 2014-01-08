82a91f13c61a0ef6fd9c1ae007b5fe3442f4c42f
Annelies Odermann <annelies@Anneliess-MacBook-Pro.local>
2014-01-08 13:04:18 -0600

    Initial commit with previous work

---
+++ withpipes.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var through = require('through');
+var trumpet = require('trumpet');
+var trump = trumpet();
+
+var count = 1;
+
+var selecting = trump.select('#essay').createStream();
+selecting.pipe(through(function (buf){
+	this.queue(buf.toString().replace(/[^>\s]\s/g, function (letter) { return  letter.substring(0, 1) + '<span>' + (count ++) + '</span> '}));
+})).pipe(selecting);
+
+var file = fs.createWriteStream('pipedessay.html');
+var essay = fs.createReadStream('essay.html');
+
+essay.pipe(trump).pipe(file);
+console.log('Something has happened');
