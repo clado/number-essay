915bb09c18353fab6fd95cfdd4221ec501bfb739
Arie Van Antwerp <arievanantwerp@gmail.com>
2014-01-21 20:05:38 -0600

    Finally added proper console loggin.

--- withpipes.js
+++ withpipes.js
@@ -6,6 +6,11 @@ var trump = trumpet();
 var count = 1;
 
 var selecting = trump.select('#essay').createStream();
+
+selecting.on('error', function() {
+ console.error('There is an error in your essay.html code. Did it get corrupeted?');
+});
+
 selecting.pipe(through(function (buf){
 	this.queue(buf.toString().replace(/[^>\s]\s/g, function (letter) { return  letter.substring(0, 1) + '<span>' + (count ++) + '</span> '}));
 })).pipe(selecting);
@@ -13,5 +18,18 @@ selecting.pipe(through(function (buf){
 var file = fs.createWriteStream('pipedessay.html');
 var essay = fs.createReadStream('essay.html');
 
+
+file.on('error', function() {
+  console.error('There was an error reading your essay.html file. Is it in the right folder?');
+});
+essay.on('error', function() {
+  console.error('There was an error writing your pipedessay.html file. Did you modify it while the program was running?');
+});
+essay.on('end', function() {
+  console.log('Your essay has now been successfully numbered. Congratulations!');
+});
+
+
+
 essay.pipe(trump).pipe(file);
-console.log('Your essay should now be successfully numbered.');
+
